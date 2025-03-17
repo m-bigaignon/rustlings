b0385e6f25fd3577242ba13851117d2fe61e9bd2
Mathias BIGAIGNON <m.bigaignon@xefi.fr>
2025-03-17 13:27:27 +0100

    Quiz 1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,13 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(apple_amount: u32) -> u32 {
+    if apple_amount > 40 {
+        apple_amount
+    } else {
+        apple_amount * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
